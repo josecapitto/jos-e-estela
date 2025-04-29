2890785ad7aa4cbe3e244c50b889196140b2ac43
José Eduardo de Abreu Capito <du3xtre@gmail.com>
2025-04-29 14:15:39 -0300

    guardar o dado Questão 2

--- funcoes.py
+++ funcoes.py
@@ -1,6 +1,6 @@
 import random
 
-def rolar_dados (n):
+def rolar_dados(n):
     lista = []
     i = 0 
     while i < n:
@@ -8,4 +8,17 @@ def rolar_dados (n):
         lista.append(num)
         i += 1 
     return lista 
+
+def guardar_dado(rolados, estoque, guardar): 
+    lista_estoque = []
+    lista_estoque.append(estoque)
+    estoque_c = estoque.copy()
+    guarda = rolados[guardar]
+    estoque_c.append(guarda)
+    lista_estoque.append(estoque_c)
+    return lista_estoque
+
+
+
+    
 
