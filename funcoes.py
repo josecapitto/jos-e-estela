3ceaa7981ea231c070a6b368268100d79aebad6c
José Eduardo de Abreu Capito <du3xtre@gmail.com>
2025-04-29 10:41:51 -0300

    rolando os dados Questão 1

--- funcoes.py
+++ funcoes.py
@@ -1,2 +1,11 @@
+import random
+
 def rolar_dados (n):
-    return n + 1 
+    lista = []
+    i = 0 
+    while i < n:
+        num = random.randint(1, 6)
+        lista.append(num)
+        i += 1 
+    return lista 
+
