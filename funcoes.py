a9de700f8a63dfc261ff17c26874ff308a8de70b
José Eduardo de Abreu Capito <du3xtre@gmail.com>
2025-04-29 18:57:55 -0300

    calcula os pontos da sequencia Q5

--- funcoes.py
+++ funcoes.py
@@ -15,6 +15,27 @@ def guardar_dado(rolados, estoque, guardar):
     del rolados[guardar]
     return [rolados, estoque]
 
+def calcula_pontos_soma(sequência):
+
+    if sequência == [1, 2, 3, 4] or sequência == [2, 3, 4, 5] or sequência == [3, 4, 5, 6]:
+        valor = 15
+    if sequência == [1, 2, 3, 4, 5] or sequência == [2, 3, 4, 5, 6]:
+        valor = 30
+    else:
+        valor = sum(sequência)
+    if len(sequência) == 5:
+        num = sequência[0]
+        for i in sequência:
+            if num != i:
+                valor = sum(sequência)
+                break
+            else:
+                valor = 50
+    return valor 
+
+
+
+
 
 
 
